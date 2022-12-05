703722442d2af797dd98625b45279e2df3ea1521
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2022-12-05 21:31:12 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.swift
@@ -0,0 +1,14 @@
+func solution(_ n:Int) -> Int{
+    var sum : Int = 0
+ 
+    if n>0{
+        for i in 1...n{
+            if n % i == 0 {
+                sum += i
+            }
+        }
+        return sum
+    }
+    return 0
+    
+}
