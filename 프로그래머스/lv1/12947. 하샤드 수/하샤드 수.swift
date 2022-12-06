bb03917d9f51491afb0f338dcce361fcae84a698
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2022-12-06 22:27:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.swift
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.swift
@@ -1,5 +1,4 @@
 func solution(_ x:Int) -> Bool {
     var add = String(x).map({Int(String($0))!}).reduce(0,+)
-    var sum = Double(x).truncatingRemainder(dividingBy: Double(add))
-    return  sum == 0 ? true : false
+    return  x%add == 0
 }
