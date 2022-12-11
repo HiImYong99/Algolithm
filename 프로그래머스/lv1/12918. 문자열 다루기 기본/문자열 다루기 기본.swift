0608440373d6a0613349336327458536e988528e
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2022-12-11 21:33:25 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func solution(_ s:String) -> Bool {
-    var nums = [0,1,2,3,4,5,6,7,8,9]
     if (s.count == 4 || s.count == 6) && s.allSatisfy({$0.isNumber}){
         return true
     }
