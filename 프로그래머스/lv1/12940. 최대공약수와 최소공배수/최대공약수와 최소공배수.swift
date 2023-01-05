b878cbe2466dda77b4b4244a96ce8c95584f614a
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2023-01-05 17:45:24 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.15 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -4,8 +4,6 @@ func solution(_ n:Int, _ m:Int) -> [Int] {
     return [gcd(n: n, m: m),lcm(n: n, m: m)]
 }
 
-
-
 func gcd(n:Int, m:Int) -> Int {
     var num1 = n
     var num2 = m
@@ -23,25 +21,14 @@ func gcd(n:Int, m:Int) -> Int {
             gcd = i
         }
     }
-    
     return gcd
 }
 
 func lcm(n:Int, m:Int) -> Int {
-    var num1 = n
-    var num2 = m
-    var temp = 0
+    let num1 = n
+    let num2 = m
     var lcm = 1
     
-    if n > m {
-        temp = num1
-        num1 = num2
-        num2 = temp
-    }
-    print(num1 / gcd(n: n, m: m))
-    print(num2 / gcd(n: n, m: m))
     lcm = gcd(n: n, m: m) * (num1 / gcd(n: n, m: m)) * (num2 / gcd(n: n, m: m))
-  
-    
     return lcm
 }
