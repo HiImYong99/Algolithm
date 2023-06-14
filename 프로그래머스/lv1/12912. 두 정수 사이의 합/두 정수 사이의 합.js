783c5ddffaa8c8e18169177a119033bd6186c7ca
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2023-06-15 00:49:10 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,20 @@
+function solution(a, b) {
+    
+    var answer = 0;
+    
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            answer += i;
+            }  
+        } 
+    else if (b < a) {
+            for (let i = b; i <= a; i++) {
+            answer += i;
+        }
+    } 
+
+    else if (a == b) {
+        answer = a;         
+    }
+        return answer;
+}
