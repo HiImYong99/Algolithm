ca359f241e685fc87c34738792055de8eb7874d2
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2023-09-25 21:01:26 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,6 +1,6 @@
 def solution(my_string, overwrite_string, s):
     answer = ''
     lst = list(my_string)
-    lst[s:s+len(overwrite_string)] =list(overwrite_string)
+    lst[s:s+len(overwrite_string)] = overwrite_string
     answer = answer.join(lst)
     return answer
