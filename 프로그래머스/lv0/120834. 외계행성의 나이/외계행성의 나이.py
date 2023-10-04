5ea4e292db4a2cf2aade466daead8c809c9bf041
HiImYong99 <97172787+HiImYong99@users.noreply.github.com>
2023-10-04 17:40:14 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,7 @@
+def solution(age):
+    answer = ''
+    alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    for s in list(str(age)):
+        answer += alphabet_list[int(s)]    
+    return answer
